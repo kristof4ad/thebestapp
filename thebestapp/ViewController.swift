63707faa3e558e4b0da371a3b7323eab1f63ff3f
kristof4ad <ikristof@ikristof.com>
2015-03-24 14:36:11 +0000

    thebestapp
    thebestapp

---
+++ thebestapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  thebestapp
+//
+//  Created by Krzysztof Dabrowski on 24/03/2015.
+//  Copyright (c) 2015 Krzysztof Dabrowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
